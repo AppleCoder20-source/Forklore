9247462146691525def5c4136cd6232861cacdaa
Mashiur <56059603+AppleCoder20-source@users.noreply.github.com>
2026-06-22 21:05:27 -0400

    Update

--- src/forklore/test_foods.py
+++
@@ -1,4 +0,0 @@
-from forklore.data.fatsecret_client import get_food_detail, find_100g_serving
-
-detail = get_food_detail(28350)        # chocolate donut
-print(find_100g_serving(detail))        # serving = works, None = no 100g
